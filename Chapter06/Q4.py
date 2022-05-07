5b123a7bdba45d391391ce31ab9ab3b7f5802ff0
Hyunji Kim <hjkim2714@gmail.com>
2022-05-07 22:08:21 +0900

    Upload Chapter06/Q4.py answer

--- Chapter06/Q4.py
+++ Chapter06/Q4.py
@@ -3,14 +3,29 @@ N, K = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-for _ in range(K):
-    b_max = max(B)
-    B.remove(b_max)
+# self
 
-    a_min_idx = A.index(min(A))
-    A[a_min_idx] = b_max
+# for _ in range(K):
+#     b_max = max(B)
+#     B.remove(b_max)
+#
+#     a_min_idx = A.index(min(A))
+#     A[a_min_idx] = b_max
+#
+#     # print(f'A: {A}')
+#     # print(f'B: {B}')
+#
+# print(sum(A))
 
-    # print(f'A: {A}')
-    # print(f'B: {B}')
+# Answer
+A.sort()
+B.sort(reverse=True)
+
+for i in range(K):
+    if A[i] < B[i]:
+        A[i], B[i] = B[i], A[i]
+
+    else:
+        break
 
 print(sum(A))
